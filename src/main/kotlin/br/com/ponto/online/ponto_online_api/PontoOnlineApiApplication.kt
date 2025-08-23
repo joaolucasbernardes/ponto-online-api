8ea37bb4da4deed1182e2e17e2e8f435092f4635
Joao Lucas <joaolucasbernardes@gmail.com>
2025-08-23 17:09:51 -0300

    feat: Initial project structure with Spring Boot

---
+++ src/main/kotlin/br/com/ponto/online/ponto_online_api/PontoOnlineApiApplication.kt
@@ -0,0 +1,11 @@
+package br.com.ponto.online.ponto_online_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PontoOnlineApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<PontoOnlineApiApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/ponto/online/ponto_online_api/PontoOnlineApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.ponto.online.ponto_online_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PontoOnlineApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

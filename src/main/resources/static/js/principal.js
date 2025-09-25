dd88d0910e809e77d3200fad9c963560680bb9e1
Joao Lucas <joaolucasbernardes@gmail.com>
2025-09-25 11:35:04 -0300

    feat: Adiciona JS para popular dados dinâmicos na tela principal

--- src/main/resources/static/js/principal.js
+++ src/main/resources/static/js/principal.js
@@ -1,4 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Página principal carregada e script principal.js executado.');
 
+    const elementoSaudacao = document.querySelector('#saudacao');
+    const elementoDataAtual = document.querySelector('#data-atual');
+
+    const nomeUsuario = "Colaborador";
+    elementoSaudacao.textContent = `Olá, ${nomeUsuario}!`;
+
+    const hoje = new Date();
+    const opcoesDeFormatacao = {
+        weekday: 'long', 
+        year: 'numeric', 
+        month: 'long', 
+        day: 'numeric'    
+    };
+
+    let dataFormatada = hoje.toLocaleDateString('pt-BR', opcoesDeFormatacao);
+    
+    dataFormatada = dataFormatada.charAt(0).toUpperCase() + dataFormatada.slice(1);
+    
+    elementoDataAtual.textContent = dataFormatada;
+
 });

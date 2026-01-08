6c93020743124aea9224f07a7a3cbbe2233010f8
Joao Lucas <joaolucasbernardes@gmail.com>
2026-01-08 15:19:44 -0300

    feat: adiciona campo role no LoginRespostaDTO

--- src/main/kotlin/br/com/ponto/online/dto/LoginRespostaDTO.kt
+++ src/main/kotlin/br/com/ponto/online/dto/LoginRespostaDTO.kt
@@ -3,6 +3,7 @@ package br.com.ponto.online.dto
 data class LoginRespostaDTO(
     val mensagem: String,
     val token: String,
-    val funcionarioId: Long,
-    val nome: String
+    val funcionarioId: Long?,
+    val nome: String,
+    val role: String
 )
